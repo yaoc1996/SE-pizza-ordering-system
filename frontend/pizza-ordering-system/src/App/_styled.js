55cab0462d3827289fb50e11d9e4555512d4da52
yaoc1996 <yaoc1996@gmail.com>
2018-03-05 14:55:01 -0500

    added sidepanel

---
+++ frontend/pizza-ordering-system/src/App/_styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const AppView = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+`;
+
+const MainView = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`
+
+export {
+  AppView,
+  MainView,
+};
