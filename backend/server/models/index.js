9d280e1778228d4333231229ba3134a3a0ac408c
yaoc1996 <yaoc1996@gmail.com>
2018-03-05 16:10:52 -0500

    removed

--- backend/server/models/index.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const basename = path.basename(module.filename);
-
-module.exports = (registerModels, config) => ({
-  models() {
-    return registerModels(__dirname, basename, config);
-  }
-})
