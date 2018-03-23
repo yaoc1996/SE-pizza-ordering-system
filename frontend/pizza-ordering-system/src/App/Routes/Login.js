21513ee6bee5901c38763d321b75a75f8a1d1629
yaoc1996 <yaoc1996@gmail.com>
2018-03-23 13:02:19 -0400

    added navigation buttons

--- frontend/pizza-ordering-system/src/App/Routes/Login.js
+++ frontend/pizza-ordering-system/src/App/Routes/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import {
   HVCenteredBox,
@@ -10,15 +10,24 @@ import {
   FFLabel,
   FFInput,
   FormButton,
+  FloatLButton,
+  FloatRButton,
+  ClickableLabel,
 } from 'styled';
 
 import postLogin from 'lib/postLogin';
 
 const Login = props => {
   const {
+    redirectDest,
     redirect,
     setAppState,
+    history,
   } = props;
+
+  const goTo = dest => () => {
+    history.push(dest);
+  }
 
   const onLogin = e => {
     e.preventDefault();
@@ -42,29 +51,40 @@ const Login = props => {
   }
 
   return (
-    <HVCenteredBox>
-      <PageHeading>Log In</PageHeading>
-      <LogoLabel>OPDS</LogoLabel>
-      <AppNameLabel>Online Pizza Delivery System</AppNameLabel>
-      <Form
-        onSubmit={onLogin} >
-        <FormField>
-          <FFLabel>Email:</FFLabel>
-          <FFInput
-            type='text'
-            name='email'
-            required />
-        </FormField>
-        <FormField>
-          <FFLabel>Password:</FFLabel>
-          <FFInput
-            type='password'
-            name='password'
-            required />
-        </FormField>
-        <FormButton>Login</FormButton>
-      </Form>
-    </HVCenteredBox>
+    <Fragment>
+      <FloatLButton
+        onClick={goTo('/home')} >Home</FloatLButton>
+      <FloatRButton
+        onClick={goTo('/signup')} >Go To Sign Up</FloatRButton>
+      <HVCenteredBox>
+        <PageHeading>Log In</PageHeading>
+        <LogoLabel>OPDS</LogoLabel>
+        <AppNameLabel>Online Pizza Delivery System</AppNameLabel>
+        <Form
+          onSubmit={onLogin} >
+          <FormField>
+            <FFLabel>Email:</FFLabel>
+            <FFInput
+              type='text'
+              name='email'
+              required />
+          </FormField>
+          <FormField>
+            <FFLabel>Password:</FFLabel>
+            <FFInput
+              type='password'
+              name='password'
+              required />
+          </FormField>
+          <FormButton>Login</FormButton>
+        </Form>
+        {
+          redirectDest === '/store' &&
+          <ClickableLabel
+            onClick={redirect} >Continue without logging in...</ClickableLabel>
+        }
+        </HVCenteredBox>
+    </Fragment>
   )
 }
 
