d5c7901e41c2832c0bcb27f8c1aa3b64e7c44a49
yaoc1996 <yaoc1996@gmail.com>
2018-03-22 23:56:00 -0400

    login route

--- frontend/pizza-ordering-system/src/App/Routes/Login.js
+++ frontend/pizza-ordering-system/src/App/Routes/Login.js
@@ -1,21 +1,71 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
 
 import {
+  HVCenteredBox,
   PageHeading,
   LogoLabel,
+  AppNameLabel,
+  Form,
+  FormField,
+  FFLabel,
+  FFInput,
+  FormButton,
 } from 'styled';
-import { AppNameLabel } from '../../styled';
 
-class Login extends Component {
-  render() {
-    return (
-      <Fragment>
-        <PageHeading>Login</PageHeading>
-        <LogoLabel>OPDS</LogoLabel>
-        <AppNameLabel>Online Pizza Delivery System</AppNameLabel>
-      </Fragment>
-    )
+import postLogin from 'lib/postLogin';
+
+const Login = props => {
+  const {
+    redirect,
+    setAppState,
+  } = props;
+
+  const onLogin = e => {
+    e.preventDefault();
+    
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    postLogin({
+      email,
+      password,
+    })
+      .then(json => {
+        if (json && json.success) {
+          setAppState({
+            user: json.user,
+          })
+          redirect();
+        }
+        console.log(json);
+      })
   }
+
+  return (
+    <HVCenteredBox>
+      <PageHeading>Log In</PageHeading>
+      <LogoLabel>OPDS</LogoLabel>
+      <AppNameLabel>Online Pizza Delivery System</AppNameLabel>
+      <Form
+        onSubmit={onLogin} >
+        <FormField>
+          <FFLabel>Email:</FFLabel>
+          <FFInput
+            type='text'
+            name='email'
+            required />
+        </FormField>
+        <FormField>
+          <FFLabel>Password:</FFLabel>
+          <FFInput
+            type='password'
+            name='password'
+            required />
+        </FormField>
+        <FormButton>Login</FormButton>
+      </Form>
+    </HVCenteredBox>
+  )
 }
 
 export default Login;
