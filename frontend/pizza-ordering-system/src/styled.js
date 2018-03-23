40e7137840b7c990602440aa97ec7c67af24df43
yaoc1996 <yaoc1996@gmail.com>
2018-03-22 22:22:50 -0400

    added styles

---
+++ frontend/pizza-ordering-system/src/styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const PageHeading = styled.h1`
+  color: #ccc;
+  font-size: 72px;
+  margin: 36px 0;
+`
+
+const LogoLabel = styled.label`
+  color: #333;
+  font-size: 18px;
+  font-weight: 700;
+  margin: 12px 0;
+`
+
+const AppNameLabel = styled.label`
+  display: block;
+  color: #ddd;
+  font-size: 12px;
+  font-weight: 700;
+  margin: 12px 0;  
+`
+
+export {
+  PageHeading,
+  LogoLabel,
+  AppNameLabel,
+}
