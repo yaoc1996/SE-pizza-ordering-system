a659952317160168342a2875da104d5fcb63f29f
yaoc1996 <yaoc1996@gmail.com>
2018-05-11 13:31:49 -0400

    heroku

--- frontend/pizza-ordering-system/src/index.js
+++ frontend/pizza-ordering-system/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'css/index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -11,4 +10,3 @@ ReactDOM.render(
   </BrowserRouter>
 , document.getElementById('root'));
 
-registerServiceWorker();
